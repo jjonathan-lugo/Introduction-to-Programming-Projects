00ed954da17f18a61c27ff9f24b231534042b013
Jonathan <jjonathan.lugo12@gmail.com>
2026-04-07 20:03:23 -0400

    Create A funtion.cpp

---
+++ American Flag/A funtion.cpp
@@ -0,0 +1,39 @@
+#include "library.h" 
+
+void draw_star(const int x, const int y, const int length) 
+{
+	set_pen_color(color::black); 
+	set_pen_width(3); 
+	move_to(x,y); 
+	set_heading_degrees(90); 
+	draw_distance(length); 
+	turn_left_by_degrees(180-108); 
+	draw_distance(length); 
+	turn_right_by_degrees(180-36); 
+	draw_distance(length); 
+	turn_left_by_degrees(180-108); 
+	draw_distance(length); 
+	turn_right_by_degrees(180-36); 
+	draw_distance(length); 
+	turn_left_by_degrees(180-108); 
+	draw_distance(length); 
+	turn_right_by_degrees(180-36); 
+	draw_distance(length); 
+	turn_left_by_degrees(180-108); 
+	draw_distance(length); 
+	turn_right_by_degrees(180-36); 
+	draw_distance(length); 
+	turn_left_by_degrees(180-108); 
+	draw_distance(length); 
+	turn_right_by_degrees(180-36); 
+	draw_distance(length); 
+}
+
+void main() 
+{ 
+	make_window(400, 400);
+draw_star(-5, 150, 50);
+draw_star(120, 150, 50);
+draw_star(250, 150, 50);
+//(x, y, size)
+}
